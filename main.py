887baa0f8f5682cc8c65855e2989f9fa1fd933ac
Administrateur <administrateur@MacBook-Air-de-Ouassim.local>
2025-12-04 09:52:01 +0100

    feature: dechatomic search function

---
+++ main.py
@@ -0,0 +1,24 @@
+def dechatomique_search(list, target):
+    not_found=True
+    start=0
+    end=len(list)-1
+    mid=(start+end)//2
+    while start<=end and not_found:
+        if list[mid]==target:
+            not_found=False
+            return mid
+        elif list[mid]<target:
+            start=mid+1
+            mid=(start+end)//2
+        else:
+            end=mid-1
+            mid=(start+end)//2  
+    return "target not found"
+
+# Example usage:
+my_list = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+target_value = 9
+result = dechatomique_search(list=my_list, target=target_value)
+print(result)          
+    
+

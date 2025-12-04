d009af7882c3b4d727b790e84e3ee798d51a4290
Administrateur <administrateur@MacBook-Air-de-Ouassim.local>
2025-12-04 09:55:31 +0100

    feature: linear search function

--- main.py
+++ main.py
@@ -14,11 +14,17 @@ def dechatomique_search(list, target):
             end=mid-1
             mid=(start+end)//2  
     return "target not found"
+def linear_search(list, target):
+    for i in range(len(list)):
+        if list[i]==target:
+            return i
+    return "target not found"
 
 # Example usage:
 my_list = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-target_value = 9
-result = dechatomique_search(list=my_list, target=target_value)
-print(result)          
-    
+target_value = 6
+dechatomique_search_result = dechatomique_search(list=my_list, target=target_value)
+linear_search_result = linear_search(list=my_list, target=target_value)
+print(f"the deachatomic search result is {dechatomique_search_result}")          
+print(f"the linear search result is {linear_search_result}")
 

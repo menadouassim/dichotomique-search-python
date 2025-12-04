33cae98770cfcde973292c0ecd606f25edabb55f
Administrateur <administrateur@MacBook-Air-de-Ouassim.local>
2025-12-04 10:09:25 +0100

    feature:added a sorted list generation function and compared the two search functions

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
+import random
+import time
 def dechatomique_search(list, target):
-    not_found=True
+    
     start=0
     end=len(list)-1
-    mid=(start+end)//2
-    while start<=end and not_found:
+    
+    while start<=end :
+        mid=(start+end)//2
         if list[mid]==target:
-            not_found=False
+            
             return mid
         elif list[mid]<target:
             start=mid+1
-            mid=(start+end)//2
+            
         else:
             end=mid-1
-            mid=(start+end)//2  
+            
     return "target not found"
 def linear_search(list, target):
     for i in range(len(list)):
         if list[i]==target:
             return i
     return "target not found"
-
+def generate_sorted_list(size):
+    return sorted(random.sample(range(1, size + 1), size))
 # Example usage:
-my_list = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-target_value = 6
+my_list = generate_sorted_list(1_000_000)
+target_value = random.randint(1, 1_000_000)
+time_start = time.time()
 dechatomique_search_result = dechatomique_search(list=my_list, target=target_value)
+time_end = time.time()
+print(f"the deachatomic search result is {dechatomique_search_result} and it took {time_end - time_start} seconds")          
+time_start = time.time()
 linear_search_result = linear_search(list=my_list, target=target_value)
-print(f"the deachatomic search result is {dechatomique_search_result}")          
-print(f"the linear search result is {linear_search_result}")
+time_end = time.time()
+print(f"the linear search result is {linear_search_result} and it took {time_end - time_start} seconds")
 
